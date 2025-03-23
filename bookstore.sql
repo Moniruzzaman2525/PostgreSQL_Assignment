ce2330abb6e64c09f9fd51efb57debb6dc53baf5
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-23 20:55:20 +0600

    order table created done

--- bookstore.sql
+++ bookstore.sql
@@ -1,6 +1,5 @@
 -- Active: 1742135091249@@127.0.0.1@5432@bookstore_db
 -- Create a "books" table
-
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -10,13 +9,20 @@ CREATE TABLE books (
     published_year INT
 );
 
--- 
-
 -- Create the "customers" table
-
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
     email VARCHAR(50) NOT NULL,
     joined_date DATE DEFAULT CURRENT_DATE
 );
+
+-- Create an "orders" table
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+    book_id INT REFERENCES books(id) ON DELETE CASCADE,
+    quantity INT CHECK (quantity > 0),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
