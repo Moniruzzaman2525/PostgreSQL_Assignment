7b039ecf7637d69d119a2a8366a9dfbc724ff10e
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-23 20:49:41 +0600

    create books query done

--- bookstore.sql
+++ bookstore.sql
@@ -1,1 +1,11 @@
 -- Active: 1742135091249@@127.0.0.1@5432@bookstore_db
+-- Create a "books" table
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    price DECIMAL(10, 2) CHECK (price >= 0),
+    stock INT CHECK (stock >= 0),
+    published_year INT
+);
